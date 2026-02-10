61042a05f3650c23d9439bb4412799a24a7700cb
HGLiu1998 <liuarthur1998@gmail.com>
2026-02-10 16:17:53 +0800

    test

--- fmha_mfma_kernel.hpp
+++ fmha_mfma_kernel.hpp
@@ -74,9 +74,9 @@ void fmha_mfma(
         }
         if (tid == 0) {
             printf("%f, %f, %f, %f\n", (float)a[0], (float)a[1], (float)a[2], (float)a[3]);
-            printf("%f, %f, %f, %f\n", (float)b[0], (float)b[1], (float)b[2], (float)b[3]);
+            printf("%f, %f, %f, %f\n", (float)b[0], b[1], b[2], b[3]);
         }
-        acc = __builtin_amdgcn_mfma_f32_16x16x16_bf16(a, b, acc, 0, 0, 0);
+        acc = __builtin_amdgcn_mfma_f32_16x16x16_bf16_1k(a, b, acc, 0, 0, 0);
     }
 
     for (int i = 0; i < 4; ++i) {
