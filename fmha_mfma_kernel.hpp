5aab19bad850f5f61a1db5c094a9d3b7a34acb17
HGLiu1998 <liuarthur1998@gmail.com>
2026-02-12 10:52:00 +0800

    K mat

--- fmha_mfma_kernel.hpp
+++ fmha_mfma_kernel.hpp
@@ -136,6 +136,8 @@ void fmha_mfma(
     }
     printf("\nFinal output:\n");
     if (tid == 0 && head_idx == 0 && batch_idx == 0)  {
-            printf("%f", (float)O_ptr[cRegLoc]);
+        for (int i = 0; i < dim_idx; ++i) {
+            printf("%f ", (float)O_ptr[i]);
+            }
     }
 }   
