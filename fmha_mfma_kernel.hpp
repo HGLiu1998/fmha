03fc7860c0ad255f0022aaf17d8a1c3e4cfe3d89
HGLiu1998 <liuarthur1998@gmail.com>
2026-02-23 15:21:42 +0800

    fmha mfma check

--- fmha_mfma_kernel.hpp
+++ fmha_mfma_kernel.hpp
@@ -105,6 +105,13 @@ void fmha_mfma(
     }
     __syncthreads();
 
+    if (tid == 0 && head_idx == 0 && batch_idx == 0)  {
+        printf("\nSoftmax %d:\n", tid);
+        for (int i = 0 ; i < seqlen_kv; i++) {
+            printf("%f, ", (float)softmax_scores[i]);
+        }
+        printf("\n");
+    }
     a = {0};
     b = {0};
 
@@ -126,6 +133,12 @@ void fmha_mfma(
             if (cRegLoc < head_dim_q) {
                 O_ptr[cRegLoc] += static_cast<half_t>(acc[i]);
             }
+        }
+        if (tid == 64 && head_idx == 0 && batch_idx == 0)  {
+            printf("\nAcc %d:\n", tid);
+            printf("%f, %f, %f, %f\n", (float)a[0], (float)a[1], (float)a[2], (float)a[3]);
+            printf("%f, %f, %f, %f\n", (float)b[0], (float)b[1], (float)b[2], (float)b[3]);
+            printf("%f, %f, %f, %f\n", (float)acc[0], (float)acc[1], (float)acc[2], (float)acc[3]);
         }
         __syncthreads();
 
